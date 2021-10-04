358c4832184146e735377ee0158f8684b297853d
Harsh Gautam <harsh.gautam_cs19@gla.ac.in>
2021-10-05 00:26:15 +0530

    Added Navbar and side bar

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.addEventListener("load", () => {
   const canvas = document.querySelector("#canvas");
   const ctx = canvas.getContext("2d");
-
+  // canvas.height = document.getElementById("canvas-contain").innerHeight;
+  // canvas.width = document.getElementById("canvas-contain").innerWidth;
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
   //variables
@@ -18,10 +19,10 @@ window.addEventListener("load", () => {
     if (!painting) return;
     ctx.lineWidth = 10;
     ctx.lineCap = "round";
-    ctx.lineTo(e.clientX, e.clientY);
+    ctx.lineTo(e.clientX - 64, e.clientY - 57);
     ctx.stroke();
     ctx.beginPath();
-    ctx.moveTo(e.clientX, e.clientY);
+    ctx.moveTo(e.clientX - 64, e.clientY - 57);
   }
   function touchstart(event) {
     startPosition(event.touches[0]);
@@ -46,4 +47,6 @@ window.addEventListener("load", () => {
 window.addEventListener("resize", () => {
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
+  // canvas.height = document.getElementById("canvas-contain").innerHeight;
+  // canvas.width = document.getElementById("canvas-contain").innerWidth;
 });
